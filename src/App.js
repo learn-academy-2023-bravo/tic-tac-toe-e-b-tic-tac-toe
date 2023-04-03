a23a1e125bbe615440f7e2e4ed61c1ade393b00a
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-04-03 16:18:15 -0700

    working on player count

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+ 
+  //This function counts player turns 
+  const [count, setCount] = useState(0)
+
+  //On clicks we need to set count plus one
+  const handleGamePlay = (clickedSquare) => {
+    let updateSquare = [...squares]
+    setCount(count + 1)
+    if(count % 2 !== 0) {
+      updateSquare[clickedSquare] = "❌"   
+    } else {
+      updateSquare[clickedSquare] = "🧿"
+    }
+      
+  }
+
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className="board">
+        {
+          squares.map((square, index) => {
+            return <Square
+            square={square}
+            index={index}
+            key={index}
+            handleGamePlay={handleGamePlay}
+            />
+          })
+        }
+      </div>
     </>
   )
 }
